a599a7b01cab57909991b283056ce784bdf5a5c0
Lucas Lin <lucas.yuchun.lin@gmail.com>
2023-03-16 03:27:08 +0800

    fix google api unknown error

--- google_service.py
+++ google_service.py
@@ -18,16 +18,18 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly', 'https://www.
 
 
 class GoogleService:
-    creds = creds = service_account.Credentials.from_service_account_info(
+    creds = service_account.Credentials.from_service_account_info(
         SERVICE_ACCOUNT_INO_DICT,
         scopes=SCOPES
     )
+    
+    calendar_service = build('calendar', 'v3', credentials=creds, static_discovery=False)
+    
+    sheet_service = build('sheets', 'v4', credentials=creds, static_discovery=False)
 
     def fetch_sheet_results(self, spread_sheet_id: str, sheet_name: str = None) -> List[list]:
-        service = build('sheets', 'v4', credentials=self.creds)
-
         # Fetch the data from the sheet
-        result = service.spreadsheets().values().get(
+        result = self.sheet_service.spreadsheets().values().get(
             spreadsheetId=spread_sheet_id,
             range=sheet_name
         ).execute()
@@ -35,12 +37,11 @@ class GoogleService:
         return [row for row in result['values'] if row]
 
     def batch_upsert_in_calendar(self, calendar_id: str, events: List[dict]):
-        service = build('calendar', 'v3', credentials=self.creds)
         try:
-            batch = service.new_batch_http_request()
+            batch = self.calendar_service.new_batch_http_request()
             
             for event in events:
-                batch.add(service.events().insert(calendarId=calendar_id, body=event))
+                batch.add(self.calendar_service.events().insert(calendarId=calendar_id, body=event))
                 
             batch.execute()
         except HttpError as e:
@@ -49,13 +50,13 @@ class GoogleService:
         print(f'{len(events)} events created: {event.get("htmlLink")}')
     
     def clean_calendar(self, calendar_id: str, to_clear_all: bool = False):
-        service = build('calendar', 'v3', credentials=self.creds)
         now = datetime.utcnow()
         now_before_one_year = now - timedelta(days=365)
         now_before_one_year = now_before_one_year.isoformat() + 'Z'  # 'Z' indicates UTC time
+        
         to_continue = True
         while to_continue:
-            events_result = service.events().list(
+            events_result = self.calendar_service.events().list(
                 calendarId=calendar_id,
                 timeMin=now_before_one_year,
                 maxResults=100,
@@ -64,16 +65,16 @@ class GoogleService:
             ).execute()
             
             events = events_result.get('items', [])
-            batch = service.new_batch_http_request()
+            batch = self.calendar_service.new_batch_http_request()
             
             for event in events:
                 if to_clear_all:
-                    batch.add(service.events().delete(calendarId=calendar_id, eventId=event['id']))
+                    batch.add(self.calendar_service.events().delete(calendarId=calendar_id, eventId=event['id']))
                 else:
                     start = event['start'].get('dateTime', event['start'].get('date'))
                     start_time = datetime.fromisoformat(start.replace('Z', '+00:00')).strftime('%Y-%m-%dT%H:%M:%S.%f')
                     if datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S.%f') < datetime.now():
-                        batch.add(service.events().delete(calendarId=calendar_id, eventId=event['id']))
+                        batch.add(self.calendar_service.events().delete(calendarId=calendar_id, eventId=event['id']))
                     
             batch.execute()
             print(f'{len(events)} events deleted')
